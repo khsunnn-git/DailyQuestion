553d1c28f7b94a30e61ef6911b48dc32512cea18
김혜선 <hyesun@gimhyeseon-ui-MacBookAir.local>
2026-03-04 01:19:26 +0900

    더보기 설정 질문 일수 표기를 누적 답변일 기준으로 변경

--- lib/features/more/more_profile_stats_store_io.dart
+++ lib/features/more/more_profile_stats_store_io.dart
@@ -1,5 +1,6 @@
 import "../../data/local_db/entities/bucket_item_entity.dart";
 import "../../data/local_db/local_database.dart";
+import "../../core/kst_date_time.dart";
 import "../question/today_question_store.dart";
 import "more_profile_stats_store.dart";
 
@@ -8,10 +9,17 @@ Future<MoreProfileStats> loadMoreProfileStats() async {
   final int bucketCount = await isar.bucketItemEntitys.count();
 
   await TodayQuestionStore.instance.initialize();
-  final int streakDays = TodayQuestionStore.instance.consecutiveRecordDays;
+  final Set<String> answeredDateKeys = <String>{};
+  for (final record in TodayQuestionStore.instance.value) {
+    final String key = (record.questionDateKey?.trim().isNotEmpty ?? false)
+        ? record.questionDateKey!.trim()
+        : kstDateKeyFromDateTime(record.createdAt);
+    answeredDateKeys.add(key);
+  }
+  final int answeredDays = answeredDateKeys.length;
 
   return MoreProfileStats(
-    questionStreakDays: streakDays > 0 ? streakDays : null,
+    questionStreakDays: answeredDays > 0 ? answeredDays : null,
     bucketCount: bucketCount,
   );
 }
